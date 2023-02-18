c297bfdbdfd7411ca02e52a14a8a15a6d519a939
jcastellanos <jcastellanos@zetti.com.ar>
2023-02-18 15:03:58 -0300

    explicacion jsx

---
+++ inicio/main.jsx
@@ -0,0 +1,12 @@
+/* JSX */
+
+//1*- expresiones solo entre llaves 
+const name = 'jhonny'
+//<h1>{name}</h1>
+
+//2*-siempre cosas que devuelvan una expresion, nada de if (porq son declaraciones)
+const element = <strong>Numero Aleatorio. {Math.random()}</strong>;
+
+//2*- atributos de los componentes en camelCase
+<button dataIcon='123'>button</button>
+
